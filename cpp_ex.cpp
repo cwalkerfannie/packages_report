2f61a734ba79594b0f88bacb058ca928178ea3c6
cwalkerfannie <109926382+cwalkerfannie@users.noreply.github.com>
2022-07-26 13:48:41 -0400

    Update cpp_ex.cpp
    Changing my function

--- cpp_ex.cpp
+++ cpp_ex.cpp
@@ -2,8 +2,10 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-void counter(int starter) {
-  for (int i = starter; i < 5; i++) {
-    Rcout i;
-  }
+double vacc3a(double age, bool female, bool ily){
+  double p = 0.25 + 0.3 * 1 / (1 - exp(0.04 * age)) + 0.1 * ily;
+  p = p * (female ? 1.25 : 0.75);
+  p = std::max(p, 0.0);
+  p = std::min(p, 1.0);
+  return p;
 }
